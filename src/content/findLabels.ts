cf05939dffed012ef9f45ddd28c3f33fb3da254e
Quinn Vaughn <qvaughn3@gmail.com>
2021-07-09 19:49:22 -0500

    Will also now search for labels via htmlFor

--- src/content/findLabels.ts
+++ src/content/findLabels.ts
@@ -5,7 +5,25 @@ export function findLabels(label: string, value: string) {
     if (labelRegex.test(label.innerText)) {
       // get the first input under a label.
       const input = label.getElementsByTagName("input")[0]
-      input.value = value
+
+      // if the label is a sibling or has a for attribute.
+      if (!input) {
+        // check if the label has a for attribute which will point
+        // us to the input.
+        if (label.hasAttribute("for")) {
+          const htmlFor = label.htmlFor
+          const input = document.getElementById(
+            htmlFor
+          ) as HTMLInputElement | null
+          if (input) {
+            input.value = value
+            break
+          }
+        }
+      } else {
+        input.value = value
+        break
+      }
     }
   }
 }
